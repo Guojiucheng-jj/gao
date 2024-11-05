359ce12d3ca696f5446e93f332437a99559881d4
Guojiucheng-jj <guojohnson80@gmail.com>
2024-11-05 18:06:49 +0800

    Update xhj.js

--- xhj.js
+++ xhj.js
@@ -1,11 +1,1 @@
-{
-          "主页url": "https://18j.tv/",
-          "直接播放": "1",
-          "嗅探词": "index.m3u8#.mp4#m3u8#.m3u8#/tos/#.flv#video/tos",
-          "图片": "data-original=\"&&\"",
-          "标题": "title=\"&&\"",
-          "链接": "href=\"&&\"",
-          "分类url": "https://18j.tv/show/{cateId}/nid/1/page/{catePg}/sid/1/",
-          "分类": "日韩$2#欧美$3#伦理$4#日本无码$14#中文字幕$15#乱伦$9#传媒剧情$12"
-          
-        }
+
